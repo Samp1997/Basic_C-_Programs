d63f3db72fbbfdcef6e2cc0716a947773621d07e
Samp1997 <sampiazza2016@gmail.com>
2023-11-22 14:47:29 -0800

    changes

--- Loops/Loops/Program.cs
+++ Loops/Loops/Program.cs
@@ -9,33 +9,44 @@ namespace Loops
         {
             Console.WriteLine("Guess a number?");
             int number = Convert.ToInt32(Console.ReadLine());
-            bool isGuessed = false;
+            bool isGuessed = number == 12;
 
-            while (isGuessed)
+            do
             {
                 switch (number)
                 {
                     case 62:
                         Console.WriteLine("You guess 62. Try again.");
+                        number = Convert.ToInt32(Console.ReadLine());
                         break;
                     case 29:
                         Console.WriteLine("You guess 29. Try again.");
+                        number = Convert.ToInt32(Console.ReadLine());
                         break;
                     case 55:
                         Console.WriteLine("You guess 55. Try again.");
+                        number = Convert.ToInt32(Console.ReadLine());
                         break;
                     case 12:
                         Console.WriteLine("You guess 12. Thats Correct!");
+                        isGuessed = true;
                         break;
                     default:
                         Console.WriteLine("You are Wrong");
+                        Console.WriteLine("Guess again");
+                        number = Convert.ToInt32(Console.ReadLine());
                         break;
 
 
                 }
-                
-                    Console.ReadLine();
             }
+
+            while (!isGuessed);
+
+            Console.Read();
+            
+                    
+            
         }
 
 
