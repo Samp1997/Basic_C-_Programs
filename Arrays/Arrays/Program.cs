878a7b931d7b97256377ce5ba1f1856c0dc8b547
Samp1997 <sampiazza2016@gmail.com>
2023-11-22 22:51:32 -0800

    changes

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -6,12 +6,36 @@ class Program
 {
     static void Main()
     {
+        int[] numArray = new int[5];
+        numArray[0] = 5;
+        numArray[1] = 2;
+        numArray[2] = 10;
+        numArray[3] =200;
+        numArray[4] = 5000;
+
+        int[] numArray1 = new int[] { 5, 2, 10, 200, 5000, 600, 2300 };
+
+        int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 };
+
+
+        numArray2[5] = 650;
+
+        Console.WriteLine("select an index");
+        int awnser = numArray1[0];
+        Console.WriteLine("You picked " + awnser);
+
+        Console.WriteLine(numArray2[5]);
+        Console.ReadLine();
+
+
         List<string> intList = new List<string>();
         intList.Add("Hello");
         intList.Add("Sam");
         intList.Remove("Sam");
 
-        Console.WriteLine(intList[0]);
+        Console.WriteLine();
+
+        byte[] byteArray = new byte[5000];
 
     }
 }
