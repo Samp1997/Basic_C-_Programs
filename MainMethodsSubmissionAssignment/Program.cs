85d389ab3227ffe505b02141f27ba767aad0a022
Samp1997 <sampiazza2016@gmail.com>
2024-04-15 13:34:59 -0700

    changes

--- MainMethodsSubmissionAssignment/Program.cs
+++ MainMethodsSubmissionAssignment/Program.cs
@@ -6,7 +6,21 @@ namespace MainMethodsSubmissionAssignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int number3 = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+            //Taking each int and applying to the methods
+            Console.WriteLine(MathOperations.Math1(number));
+            Console.ReadLine();
+
+            Console.WriteLine(MathOperations.Math2(number2));
+            Console.ReadLine();
+
+            Console.WriteLine(MathOperations.Math3(number.ToString(number3));
+            Console.ReadLine();
+
         }
     }
 }
