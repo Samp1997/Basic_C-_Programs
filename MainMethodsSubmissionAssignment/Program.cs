a08b5b5058bb298c77a07920f940140e12f37335
Samp1997 <sampiazza2016@gmail.com>
2024-04-15 12:55:10 -0700

    changes

---
+++ MainMethodsSubmissionAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MainMethodsSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
