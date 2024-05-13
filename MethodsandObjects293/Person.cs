8d3b80209b7df32859ce176a371c0c206f69c1d4
Samp1997 <sampiazza2016@gmail.com>
2024-05-13 16:29:55 -0700

    changes

---
+++ MethodsandObjects293/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodsandObjects293
+{
+    class Person
+    {
+        public string FirstName;
+
+        public string LastName;
+
+        public void SayName()
+        {
+            Console.WriteLine("Name :  "  + FirstName + " " + LastName);  
+        }
+
+
+    }
+
+    class Employee : Person
+    {
+        public int id;
+        
+
+        
+    }
+}
--- MethodsandObjects293/Program.cs
+++ MethodsandObjects293/Program.cs
@@ -2,11 +2,12 @@
 
 namespace MethodsandObjects293
 {
-    class Program
+    class Program 
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Employee Obj1 = new Employee() { FirstName = "Sample", LastName = "Student", id = 5 };
+            Obj1.SayName();
         }
     }
 }
