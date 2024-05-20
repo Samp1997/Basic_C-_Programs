669b7577636fce46b0c7870189ad86e498e704dd
Samp1997 <sampiazza2016@gmail.com>
2024-05-19 22:39:29 -0700

    changes

---
+++ AbstractClass303/People.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClass303
+{
+    public abstract class People 
+    {
+        public string FirstName;
+
+        public string LastName;
+
+        public void SayName()
+        {
+            Console.WriteLine("Name :  " + FirstName + " " + LastName);
+        }
+
+
+    }
+
+     class Employee : People
+    {
+        public int id;
+
+        public void SayName()
+        {
+            Console.WriteLine("Name :  " + FirstName + " " + LastName);
+        }
+
+    }
+}
--- AbstractClass303/Program.cs
+++ AbstractClass303/Program.cs
@@ -6,7 +6,9 @@ namespace AbstractClass303
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Employee Obj1 = new Employee() { FirstName = "Sample", LastName = "Student", id = 5 };
+            Obj1.SayName();
+
         }
     }
 }
