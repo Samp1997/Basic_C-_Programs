096fa49c554e40f1ab92aa78fa8911a5641b6a74
Samp1997 <sampiazza2016@gmail.com>
2024-06-02 18:15:18 -0700

    changes

---
+++ DateTimeAssignment371/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DateTimeAssignment371
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- Twentyonealltogether/Program.cs
+++ Twentyonealltogether/Program.cs
@@ -8,6 +8,7 @@ namespace Twentyonealltogether
         {
             Console.WriteLine("Welcome to the Gran Hotel and Casino, Let's start by telling me your name.");
             string playerName = Console.ReadLine();
+            
             Console.WriteLine("And how much money did you bring today?");
             int bank = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
