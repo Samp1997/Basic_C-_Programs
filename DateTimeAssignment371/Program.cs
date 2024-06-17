9b53bc02a323b0fff6bdd0500ce110478189c2ec
Samp1997 <sampiazza2016@gmail.com>
2024-06-17 00:26:14 -0700

    changes

--- DateTimeAssignment371/Program.cs
+++ DateTimeAssignment371/Program.cs
@@ -7,19 +7,22 @@ namespace DateTimeAssignment371
         static void Main(string[] args)
         {
 
-
-            DateTime currentTime = new DateTime(2024, 6, 2, 10, 02, 00);
-            DateTime timePassed = new DateTime(2024, 7, 3, 12, 15, 00);
+            DateTime dateTime = new DateTime(2024, 6, 2, 10, 02, 00);
+            DateTime currentTime = new DateTime(DateTime.Now);
+            DateTime timePassed = new DateTime();
             Console.WriteLine(currentTime);
             Console.ReadLine();
             Console.WriteLine("Enter a number");
-            
+            Console.ReadLine();
+            Console.WriteLine("How many many hours till your ready?");
 
 
 
 
 
-            TimeSpan amountOfTime =  currentTime - timePassed;
+
+
+            TimeSpan amountOfTime = timePassed - currentTime;
 
             Console.WriteLine(amountOfTime);
 
