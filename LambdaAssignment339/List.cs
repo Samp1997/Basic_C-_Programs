7448dc0b3ddeb737d7bf61984668536f2f100459
Samp1997 <sampiazza2016@gmail.com>
2024-06-17 00:04:52 -0700

    changes

---
+++ LambdaAssignment339/List.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LambdaAssignment339
+{
+    internal class List<T>
+    {
+        public List()
+        {
+        }
+
+        internal void Add(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- LambdaAssignment339/Program.cs
+++ LambdaAssignment339/Program.cs
@@ -6,19 +6,34 @@ namespace LambdaAssignment339
     {
         static void Main(string[] args)
         {
+            Employee employee = new Employee();
             List<Employee> employeelist = new List<Employee>();
 
+            int count = employee.Employees.Count(x => x.list == FirstName.List );
 
-            employeelist.Add("Joe");
-            employeelist.Add("Joe");
-            employeelist.Add("Kevin");
-            employeelist.Add("Micky");
-            employeelist.Add("caral");
-            employeelist.Add("frank");
-            employeelist.Add("ian");
-            employeelist.Add("liam");
-            employeelist.Add("jake");
-            employeelist.Add("mick");
+            List<Employee> newList = employee.FirstName.Where(x => x.FirstName == Employee.FirstBame).ToLsit();
+
+            foreach (Employee in newList)
+            {
+                Console.WriteLine(employee.FirstName);
+            }
+
+            Employee.List.
+         
+
+            
+
         }
     }
 }
+
+//employeelist.Add("Joe");
+//employeelist.Add("Joe");
+//employeelist.Add("Kevin");
+//employeelist.Add("Micky");
+//employeelist.Add("caral");
+//employeelist.Add("frank");
+//employeelist.Add("ian");
+//employeelist.Add("liam");
+//employeelist.Add("jake");
+//employeelist.Add("mick");
