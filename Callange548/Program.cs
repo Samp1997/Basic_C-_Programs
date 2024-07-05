f27609416a2479c6f7c9e994ab125d79b3e00b19
Samp1997 <sampiazza2016@gmail.com>
2024-07-05 15:27:38 -0700

    update

--- Callange548/Program.cs
+++ Callange548/Program.cs
@@ -9,12 +9,15 @@ using System.Threading.Tasks;
 
 namespace Callange548
 {
+
     public class Program
     {
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
+
+        
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
