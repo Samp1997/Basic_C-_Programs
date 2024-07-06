a2fca51b7fe27f7fffba78cc9aef7f756f4c32d6
Samp1997 <sampiazza2016@gmail.com>
2024-07-05 17:41:48 -0700

    update

---
+++ FinalChallange/SchoolContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalChallange
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        using (var ctx = new SchoolContext())
+        {
+            var stud = new Student() { StudentName = "Bill" };
+
+            ctx.Students.Add(stud);
+            ctx.SaveChanges();
+        }
+    }
+}
+}
